812b44d2678216b69848990abbe29de447719d31
sunhoh <dltjsgho94@gmail.com>
2024-07-18 13:30:16 +0900

    fix: eslint-plugin-boundaries

---
+++ packages/eslint-config/README.md
@@ -0,0 +1,15 @@
+# eslint-plugin-boundaries
+
+ex : [index, {...obj}]
+### index 
+0 - off: 규칙을 끕니다.
+1 - warn: 경고를 발생시킵니다. 코드가 실행되지만, 경고 메시지가 표시됩니다.
+2 - error: 오류를 발생시킵니다. 코드가 실행되지 않으며, 오류 메시지가 표시됩니다.
+
+### obj
+default : 'allow' or 'disallow' 
+message
+rules : []
+
+
+[eslint-plugin-boundaries-github](https://github.com/javierbrea/eslint-plugin-boundaries?tab=readme-ov-file#main-format-of-rules-options)
--- packages/eslint-config/rules/front/import.js
+++ packages/eslint-config/rules/front/import.js
@@ -1,60 +1,62 @@
 module.exports = {
+    env: {
+      "browser": true,
+      "es2021": true,
+      "node": true,
+      "es6": true
+    },
+    plugins: ["import", "boundaries"],
     extends: [
+        "plugin:boundaries/recommended",
         "eslint:recommended",
         "plugin:import/recommended",
         "plugin:import/errors",
         "plugin:import/warnings"
     ],
-    plugins: ["import", "boundaries"],
     rules:{
-        "boundaries/element-types": [
-            2,
+      "boundaries/element-types": [
+        2,
+        {
+          "default": "disallow",
+          "message": "${file.type} is not allowed to import ${dependency.type}",
+          "rules": [
             {
-              "default": "disallow",
-              "rules": [
-                {
-                  "from": "shared/*",
-                  "allow": []
-                },
-                {
-                  "from": "widgets/*",
-                  "allow": []
-                },
-                {
-                  "from": "features",
-                  "allow": ["shared", "widgets", "features"]
-                },
-                {
-                  "from": "entities",
-                  "allow": ["shared", "widgets", "features", "entities"]
-                },
-                {
-                  "from": "pages",
-                  "allow": ["shared", "widgets", "features", "entities", "pages"]
-                },
-                {
-                  "from": "app",
-                  "allow": ["shared", "widgets", "features", "entities", "pages", "app"]
-                }
-              ]
+              "from": ["app"],
+              "allow": ["pages", "entities", "features", "widgets", "shared"]
+            },
+            {
+              "from": ["pages"],
+              "allow": ["entities", "features", "widgets", "shared"]
+            },
+            {
+              "from": ["entities"],
+              "allow": ["features", "widgets", "shared"]
+            },
+            {
+              "from": ["features"],
+              "allow": ["widgets", "shared"]
+            },
+            {
+              "from": ["widgets"],
+              "allow": ["shared"]
+            },
+            {
+              "from": ["shared"],
+              "disallow": ["app", "pages", "entities", "features", "widgets"]
             }
-          ],
-          "import/no-extraneous-dependencies": ["error", { "devDependencies": true }]
+          ]
+        }
+      ],
+      "import/no-extraneous-dependencies": ["error", { "devDependencies": true }]
     },
     settings: {
-        "boundaries/elements": [
-          { "type": "app", "pattern": "src/app/*" },
-          { "type": "pages", "pattern": "src/pages/*" },
-          { "type": "entities", "pattern": "src/entities/*" },
-          { "type": "features", "pattern": "src/features/*" },
-          { "type": "widgets", "pattern": "src/widgets/*" },
-          { "type": "shared", "pattern": "src/shared/*" }
-        ],
-        "import/resolver": {
-          "node": {
-            "paths": ["src"],
-            "extensions": [".js", ".jsx", ".ts", ".tsx"]
-          }
-        }
-      }
+      "boundaries/elements": [
+        { "type": "app", "pattern": "src/app/**" },
+        { "type": "pages", "pattern": "src/pages/**" },
+        { "type": "entities", "pattern": "src/entities/**" },
+        { "type": "features", "pattern": "src/features/**" },
+        { "type": "widgets", "pattern": "src/widgets/**" },
+        { "type": "shared", "pattern": "src/shared/**" }
+      ]
+  }
 }
